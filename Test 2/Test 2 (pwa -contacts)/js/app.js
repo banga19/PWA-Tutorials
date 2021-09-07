255c7d5d76b0d3d4b4182df6fc5ddc0df43e1846
banga19 <62591509+banga19@users.noreply.github.com>
2021-09-07 10:20:45 +0300

    Delete Test 2/Test 2 (pwa -contacts) directory

--- Test 2/Test 2 (pwa -contacts)/js/app.js
+++
@@ -1,6 +0,0 @@
-//the code below is used to check if service worker for the app is registered 
-if('serviceWorker' in navigator){
-    navigator.serviceWorker.register('/sw.js')
-    .then((reg) => console.log('service Worker Registered', reg))
-    .catch((err) => console.log('Service Worker is NOT registered', err))
-}
